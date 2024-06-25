3ba7ea23460e51daeb4544c54e3c501576a053d2
deenu0224 <deenu0224@gmail.com>
2024-06-25 09:38:10 +0900

    add auto restart bash script. run.sh

---
+++ DemoCannon/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true; do
+	echo "Starting Program..."
+	./DemoCannon
+
+	echo "Program is shutdown. Program restart in 5 seconds..."
+	echo "If you want do stop this program, Press Ctrl+C."
+
+	sleep 5
+done
+
